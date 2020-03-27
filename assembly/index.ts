b8e8507e6a49bd414b45f23143c0d2c7b19ff5be
aaron <aaronmunstersbxl@gmail.com>
2020-03-27 21:27:13 +0100

    Updated information, no new functionality

--- assembly/index.ts
+++ assembly/index.ts
@@ -4,11 +4,20 @@
 
 type Function<RetTyp, InTyp> = (...args: InTyp[]) => RetTyp;
 
+/*
 function apply<RetTyp, InTyp>(
   f: Function<RetTyp, InTyp>, // the provided function
   ...args: InTyp[] // optional extra arguments
 ): RetTyp {
   return f(...args);
+}
+*/
+
+function singleApply<RetTyp, InTyp>(
+  f: Function<RetTyp, InTyp>, // the provided function
+  arg: InTyp // optional extra argument
+): RetTyp {
+  return f(arg);
 }
 
 /* ######################
@@ -21,13 +30,15 @@ function squareNumber(n: i32): i32 {
 
 // #### Here the source code transformation should take place
 function square(n: i32, idx: i32, arr: Array<i32>): i32 {
-  return apply<i32, i32>(squareNumber, n);
-  // return squareNumber(n);
+  return squareNumber(n); // Uninstrumented, 'empty'
+  // return singleApply<i32, i32>(squareNumber, n); // This is already possible
+  // return apply<i32, i32>(squareNumber, n); // This would be better
+  // return apply<i32, i32, i64[], i32[], i32>(exampleFunction, n, [0,1,2,3], arr, idx); // This would be ideal
 }
 
 export function squaresSum(n: i32): i32 {
-  return new Array<i32>(n)
-    .map<i32>((n, i, a) => i)
-    .map<i32>(square)
-    .reduce((prev, curr) => prev + curr, 0);
+  return new Array<i32>(n) // for n == 4: [*, *, *, *]
+    .map<i32>((n, i, a) => i) // [0, 1, 2, 3]
+    .map<i32>(square) // [0, 1, 4, 9]
+    .reduce((prev, curr) => prev + curr, 0); // 15
 }
--- program.js
+++ program.js
@@ -1,12 +1,20 @@
+const assert = require("assert").strict;
 const wasmModule = require("./index");
 
+let wasmVal;
+let jsVal;
 for (let i = 1; i < 100; i++) {
-  console.log(wasmModule.squaresSum(i));
+  wasmVal = wasmModule.squaresSum(i);
+  jsVal = new Array(i)
+    .fill(null)
+    .map((n, i) => i * i)
+    .reduce((p, n) => p + n);
 
-  console.log(
-    new Array(i)
-      .fill(null)
-      .map((n, i) => i * i)
-      .reduce((p, n) => p + n)
+  assert.equal(
+    wasmVal,
+    jsVal,
+    "the WASM module and JS code should provide same output"
   );
 }
+
+console.log("All tests passed, everything is fine");

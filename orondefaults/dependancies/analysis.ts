00ce264999ffc0ac9300478ee4f5008a3e2a3fa3
aaronmunsters <aaronmunsters@github.com>
2020-05-10 23:01:49 +0200

    Added empty example for generic apply

--- orondefaults/dependancies/analysis.ts
+++ orondefaults/dependancies/analysis.ts
@@ -2,6 +2,7 @@ import {
   dynamicPropertyRead,
   dynamicPropertyWrite,
   ArgsBuffer,
+  Types,
 } from "./analysisDependancies";
 
 export { dynamicPropertyRead, dynamicPropertyWrite };
@@ -24,7 +25,7 @@ export interface Oron {
     offset: usize
   ): void;
 
-  genericApply(fptr: usize, args: ArgsBuffer): void;
+  genericApply(fname: string, fptr: usize, args: ArgsBuffer): void;
 }
 
 export class OronAnalysis implements Oron {
@@ -50,7 +51,90 @@ export class OronAnalysis implements Oron {
     dynamicPropertyWrite<ClassInstance, Value>(classInstance, value, offset);
   }
 
-  genericApply(fptr: usize, args: ArgsBuffer): void {
+  genericApply(fname: string, fptr: usize, args: ArgsBuffer): void {
     /* Analysis would go here */
+    for (let argIdx = 0; argIdx < args.argsAmount; argIdx++) {
+      switch (args.dynamicTypes[argIdx]) {
+        case Types.i32:
+          null;
+          break;
+        case Types.u32:
+          null;
+          break;
+        case Types.i64:
+          null;
+          break;
+        case Types.u64:
+          null;
+          break;
+        case Types.f32:
+          null;
+          break;
+        case Types.f64:
+          null;
+          break;
+        case Types.v128:
+          null;
+          break;
+        case Types.i8:
+          null;
+          break;
+        case Types.u8:
+          null;
+          break;
+        case Types.i16:
+          null;
+          break;
+        case Types.u16:
+          null;
+          break;
+        case Types.bool:
+          null;
+          break;
+        case Types.isize:
+          null;
+          break;
+        case Types.usize:
+          null;
+          break;
+        case Types.void:
+          null;
+          break;
+        case Types.anyref:
+          null;
+          break;
+        case Types.number:
+          null;
+          break;
+        case Types.boolean:
+          null;
+          break;
+        case Types.classInstance:
+          switch (args.classIds[argIdx]) {
+            case idof<ArrayBuffer>():
+              null;
+              break;
+            case idof<DataView>():
+              null;
+              break;
+            case idof<Date>():
+              null;
+              break;
+            case idof<Error>():
+              null;
+              break;
+            case idof<String>():
+              null;
+              break;
+            /* Possible to include idof<Array<String>>, though finite, all combinations not included here */
+            /* */
+            default:
+              // Encountered a classInstance, type unspecified in analysis
+              null;
+              break;
+          }
+          break;
+      }
+    }
   }
 }
